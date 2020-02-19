27304861f32fa9e48a29a33ddd0ab42907ea7a95
imcdonald1 <imcdonald1@mail.csuchico.edu>
2020-02-19 14:40:45 -0800

    added authentication urls

--- grocery_gen/urls.py
+++ grocery_gen/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from grocery_gen_app.views import home_view
+from django.urls import path, include
+from grocery_gen_app.views import *
 
 urlpatterns = [
 	path('', home_view, name='home'),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', register_user, name='register'),
 ]
